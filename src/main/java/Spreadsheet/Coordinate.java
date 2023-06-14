5a37e30691b12b7d96969bd9fab97fa10699d223
Simen Dymbe <simen.dymbe@gmail.com>
2023-06-14 17:29:29 +0200

    Fix hash error in cell id contructor

--- src/main/java/Spreadsheet/Coordinate.java
+++ src/main/java/Spreadsheet/Coordinate.java
@@ -38,6 +38,7 @@ public class Coordinate {
         // -1 because 0-indexing
         col -= 1;
         row = Integer.parseInt(rowString) - 1;
+        this.hashCode = Objects.hash(row, col);
     }
 
 
