aa99833ff9ea5e253bd3c4be221dc3d35eb7a49e
Simen Dymbe <simen.dymbe@gmail.com>
2023-06-14 21:58:04 +0200

    Add excecution to some cmds

--- src/main/java/Spreadsheet/Controller.java
+++ src/main/java/Spreadsheet/Controller.java
@@ -1,23 +1,67 @@
 package Spreadsheet;
 
 import Spreadsheet.Cmd.Cmd;
+import Spreadsheet.Cmd.ECmd;
 
 public class Controller {
+    private Spreadsheet spreadsheet;
     private ContentFactory contentFactory;
+    private UI ui;
+    private FileManager fileManager;
 
     public Controller() {
+        spreadsheet = new Spreadsheet();
         contentFactory = new ContentFactory();
+        ui = new UI();
+        fileManager = new FileManager();
     }
 
-    public void run() {}
-    private void processUserCommand(Cmd cmd){}
-    private void createNewSpreadsheet(){}
-    private void loadExistingSpreadsheet(String filepath){}
+    public void run() {
+        Spreadsheet ss = null;
+        Cmd cmd = null;
+
+        while(true) {
+            try {
+                cmd = ui.askForCmd();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            processUserCommand(cmd);
+        }
+
+    }
+    private void processUserCommand(Cmd cmd){
+        switch (cmd.getType()) {
+            case "RF":
+
+                break;
+            case "C":
+                createNewSpreadsheet();
+                break;
+            case "E":
+                modifyCellContent(((ECmd) cmd).getCellID(), ((ECmd) cmd).getContentStr());
+                break;
+            case "L":
+
+                break;
+            case "S":
+
+                break;
+        }
+    }
+
+    private void createNewSpreadsheet(){
+        spreadsheet = new Spreadsheet();
+    }
+    private void loadExistingSpreadsheet(String filepath) {
+
+    }
     private void saveSpreadsheet(String filename){}
-    private void modifyCellContent(Spreadsheet ss, String cellId, String newValue){
+    private void modifyCellContent(String cellId, String newValue){
         Coordinate cellCoor = new Coordinate(cellId);
         Content newContent = contentFactory.createContent(newValue);
-        ss.updateContent(cellCoor, newContent);
+        spreadsheet.updateContent(cellCoor, newContent);
     }
 
     //private double computeFormula(Cell cell){}
